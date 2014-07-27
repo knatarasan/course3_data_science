e750d98a3dc48bd0f6e75c6f4aa8bcb238eb8c08
knatarasan <kannappan_natarasan@yahoo.com>
2014-07-26 21:17:34 -0500

    After updating setwd() option

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,25 @@
 #Set working directory
-setwd("/home/kannappan/r/course3/UCI_HAR_Dataset")
+#setwd("/home/kannappan/r/course3")
+
+setwd(".")
 
 #Read train data
-Train.core<-read.table("./train/X_train.txt")
+Train.core<-read.table("train/X_train.txt")
 
 
-Train.labels<-read.table("./train/y_train.txt",col.names="lab")
+Train.labels<-read.table("train/y_train.txt",col.names="lab")
 
-Train.subject<-read.table("./train/subject_train.txt",,col.names="sub")
+Train.subject<-read.table("train/subject_train.txt",,col.names="sub")
 
 Train<-cbind(Train.subject,Train.labels,Train.core)
 
 
 #Read test data
-Test.core<-read.table("./test/X_test.txt")
+Test.core<-read.table("test/X_test.txt")
 
-Test.labels<-read.table("./test/y_test.txt",col.names="lab")
+Test.labels<-read.table("test/y_test.txt",col.names="lab")
 
-Test.subject<-read.table("./test/subject_test.txt",col.names="sub")
+Test.subject<-read.table("test/subject_test.txt",col.names="sub")
 
 Test<-cbind(Test.subject,Test.labels,Test.core)
 
@@ -25,7 +27,7 @@ All_data<-rbind(Train,Test)
 
 
 #Read features
-features<-read.table("./features.txt",stringsAsFactors=FALSE)
+features<-read.table("features.txt",stringsAsFactors=FALSE)
 
 #take only mean std
 features.me.std<-features[grep("mean\\(\\)|std\\(\\)",features$V2),]
@@ -34,7 +36,7 @@ features.me.std<-features[grep("mean\\(\\)|std\\(\\)",features$V2),]
 data.mean.std <- All_data[, c(1, 2, features.me.std$V1+2)]
 
 #Read activity labels
-act.labels<-read.table("./activity_labels.txt")
+act.labels<-read.table("activity_labels.txt")
 
 #Assign values to labels
 data.mean.std$lab <- act.labels[data.mean.std$lab, 2]
@@ -54,7 +56,7 @@ colnames(data.mean.std)<-colnames.All_data
 final.data<-aggregate(data.mean.std[,3:ncol(data.mean.std)],by=list(subject=data.mean.std$subject,label=data.mean.std$label),mean)
 
 #Write data to file
-write.table(final.data,"/home/kannappan/r/course3/final_data.txt",row.names=F,col.names=F,quote=FALSE)
+write.table(final.data,"final_data2.txt",row.names=F,col.names=F,quote=FALSE)
 
 
 
